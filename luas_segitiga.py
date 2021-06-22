2853154c1ff09ff5dd796be7350a37fd625db2ad
Nasrullah Pandu Dewantara <70211529+Nasrulpandud@users.noreply.github.com>
2021-06-23 00:09:45 +0700

    tambah program luas segitiga

---
+++ luas_segitiga.py
@@ -0,0 +1,8 @@
+print("=====Program Luas Segitiga======\nSelamat Mencoba\nJangan digunakan saat ujian")
+alas = int(input("Masukan alas:"))
+tinggi = int(input("Masukan tinggi:"))
+
+luas= 0.5*alas*tinggi 
+print("luas segitiga adalah ",luas)
+
+
